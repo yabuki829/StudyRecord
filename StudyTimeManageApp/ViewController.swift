5e1b0b5a3806555ad87436ad8abf8139fcaa7cb6
Yabuki <syodai829@icloud.com>
2021-12-09 14:28:15 +0900

    Initial Commit

---
+++ StudyTimeManageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyTimeManageApp
+//
+//  Created by Yabuki Shodai on 2021/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
